f32f9230dc4365c80e10c1c55685934cc1ce16f6
NateBalcha <natebalcha@gmail.com>
2023-09-28 12:44:11 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/simple_ecommerce" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/store" element={<Shop />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
